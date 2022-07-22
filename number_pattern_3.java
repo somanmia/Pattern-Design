30ba698a70d6a25b6a1edebef9d72b315e826ea8
Md:Soman Mia <59284020+somanmia@users.noreply.github.com>
2022-07-22 11:44:11 +0000

    Create number_pattern_3.java

---
+++ number_pattern_3.java
@@ -0,0 +1,20 @@
+
+package Apps_Practise;
+import java.util.Scanner;
+public class number_pattern_2{
+    public static void main(String[]args){
+        Scanner input=new Scanner(System.in);
+        int row,col,n;
+        System.out.print("Enter N:");
+        n=input.nextInt();
+        for(row=1;row<=n;row++){
+           for(col=1;col<=n;col++){
+               System.out.print(""+col);
+               if(n!=col){
+                   System.out.print(" ");
+               }
+           }
+           System.out.print("\n");
+        }
+    }
+}
